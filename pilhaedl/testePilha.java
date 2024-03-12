4ddbe74629ee836ec82e64be70ba1e5103c66531
Débora Rodrigues <50723433+deborasamara@users.noreply.github.com>
2024-03-12 09:53:58 -0300

    Update testePilha.java

--- pilhaedl/testePilha.java
+++ pilhaedl/testePilha.java
@@ -15,18 +15,33 @@ public class testePilha {
 		int var_temporaria = null; 
 		
 		while(res != -1){ // ficar dando push e verificando 
+			bool maior = false;
 			System.out.println("Digite a energia da casa "+i);
 			i++;
 			Scanner scanner = new Scanner(System.in); 
 			int energia = scanner.nextInt();
 			res = energia;
 			pp.push(energia);
+			//bool permissao_apagar = false;
 			// comparar top de agora com var_temporaria
 			if(var_temporaria != null){ // não é o primeiro numero 
-				// comparar com o anterior 
+				// comparar com o anterior
+				if(pp.top()>var_temporaria){ // se for maior que o anterior, apaga e cria linha de força
+					//permissao_apagar = true;
+					System.out.println(pp.top());
+					
+				}else if(pp.top()<var_temporaria){ // se for menor que o anterior
+					//permissao_apagar = true;
+					System.out.println(var_temporaria);
+					System.out.println(pp.top());
+					pp.pop();
+					pp.pop();
+				}
+				
 				
 			}else{ // primeiro número 
 				// primeiro número do array da primeira linha de força.
+				// não faz nada
 			}
 
 			// nova atribuição ao var_temporaria
