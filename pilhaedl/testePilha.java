acd3e2ea5d75ea3e406a14e6a5a7871a47c0803e
Débora Rodrigues <50723433+deborasamara@users.noreply.github.com>
2024-03-24 16:29:16 -0300

    troca po ArrayList e Questão printando as linhas de forças

--- pilhaedl/testePilha.java
+++ pilhaedl/testePilha.java
@@ -1,57 +1,95 @@
+
 // FAZENDO A QUESTÃO DA ENERGIA
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class testePilha {
 
-	public static void main(String[] args) {	
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
 
 		PilhaArray pp = new PilhaArray(1);
-		
-		// criar condição de parada ex -1
-		int res = 1;
-		int i = 1;
 
-		// variável temporária que guarda o valor anterior
-		int var_temporaria = null; 
-		
-		while(res != -1){ // ficar dando push e verificando 
-			bool maior = false;
-			System.out.println("Digite a energia da casa "+i);
-			i++;
-			Scanner scanner = new Scanner(System.in); 
+		System.out.println("Digite a quantidade de casas");
+
+		int qtd_casas = scanner.nextInt();
+
+		ArrayList<Integer> casas_energias = new ArrayList<Integer>();
+
+		for (int i = 0; i < qtd_casas; i++) {
+			System.out.println("Digite a energia da casa : " + (i + 1));
 			int energia = scanner.nextInt();
-			res = energia;
-			pp.push(energia);
-			//bool permissao_apagar = false;
-			// comparar top de agora com var_temporaria
-			if(var_temporaria != null){ // não é o primeiro numero 
-				// comparar com o anterior
-				if(pp.top()>var_temporaria){ // se for maior que o anterior, apaga e cria linha de força
-					//permissao_apagar = true;
-					System.out.println(pp.top());
-					
-				}else if(pp.top()<var_temporaria){ // se for menor que o anterior
-					//permissao_apagar = true;
-					System.out.println(var_temporaria);
-					System.out.println(pp.top());
-					pp.pop();
-					pp.pop();
-				}
-				
-				
-			}else{ // primeiro número 
-				// primeiro número do array da primeira linha de força.
-				// não faz nada
-			}
-
-			// nova atribuição ao var_temporaria
-			var_temporaria = pp.top();
-
-			//System.out.println(pp.top()); - teste para ver se está inserindo
+			casas_energias.add(energia);
+			System.out.println();
 		}
-		// Criar as linhas de força
-		// ideia1: usar array para dar input nos elementos das energias e criar pilhas das energias de força e printar seus valores(usando as funcionalidades pop, top, push, etc, instanciando as novas pilhas 
-		// idea2: dar push na pilha, usar o top e pop e guardar o elemento anterior numa varíavel para verificar se precisa de uma nova linha de força. 
+		while (!casas_energias.isEmpty()) { // enquanto ainda houver casas para analisar
+			// ex P={3, 6, 2, 7, 5}
+			// ex P={8,6,10,4,1,5,3}
+			ArrayList<Integer> e_remover = new ArrayList<>();
+			int anterior = casas_energias.get(0); 
+			System.out.print("[");
+			boolean maior = false;
+			if (casas_energias.size() == 1) {
+				System.out.print(casas_energias.get(0));
+				casas_energias.remove(0);
+			}
+			for (int i = 1; i < casas_energias.size(); i++) { // LOOPING FORMAR PILHA
+
+				if (anterior > casas_energias.get(i) && maior == false) { // se já encontrou o maior primeiro número da
+																			// lista, ir dando push nos menores a ele
+					// se a energia da anterior for menor, então
+					maior = true;
+					pp.push(casas_energias.get(i - 1)); // primeira posição
+					System.out.print(pp.top() + ", ");
+					pp.push(casas_energias.get(i));
+					anterior = casas_energias.get(i); // posição atual (segundo número que vai ser comparado)
+					e_remover.add(casas_energias.get(i - 1));
+					e_remover.add(casas_energias.get(i));
+					if (i == (casas_energias.size() - 1)) {
+						System.out.print(pp.top() + "");
+					} else {
+						System.out.print(pp.top() + ", ");
+					}
+
+				} else if (anterior < casas_energias.get(i) && maior == false) {
+					// se a energia da anterior não for maior, então começa uma pilha na atual
+					maior = true;
+					pp.push(casas_energias.get(i));
+					anterior = casas_energias.get(i); // anterior vai ser o atual
+					e_remover.add(casas_energias.get(i));
+					if (i == (casas_energias.size() - 1)) {
+						System.out.print(pp.top() + "");
+					} else {
+						System.out.print(pp.top() + ", ");
+					}
+
+					// já encontrou o maior e agora está comparando com o anterior pra ver se é
+					// menor para adicionar
+				} else if (anterior > casas_energias.get(i) && maior == true) { // 7 > 5
+					pp.push(casas_energias.get(i));
+					anterior = casas_energias.get(i);
+					e_remover.add(casas_energias.get(i));
+					if (i == (casas_energias.size() - 1)) {
+						System.out.print(pp.top() + "");
+					} else {
+						System.out.print(pp.top() + ", ");
+					}
+				} else if (anterior < casas_energias.get(i) && maior == true) { // 7 < 5
+					anterior = casas_energias.get(i);
+				}
+			}
+			System.out.print("]\n");
+			// remover os elementos que formaram a pilha:
+			casas_energias.removeAll(e_remover);
+		}
+		/*
+		 * Acabando de printar as pilhas, DESENVOLVA UMA VERSÃO COM ARRAY, UTILIZANDO A
+		 * ESTRATÉGIA DE DUPLICAÇÃO
+		 * EM CASO DE ENCHIMENTO DO ARRAY E CASO O ARRAY CHEGUE ATÉ 25% DE OCUPAÇÃO,
+		 * O MESMO, DEVE SER REDUZIDO PELA METADE
+		 */
+		
 
 	}
 }
