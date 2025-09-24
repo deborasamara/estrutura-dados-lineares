498d2467eaffadf14d2a4154f4031e183dcc7a89
Débora Rodrigues <50723433+deborasamara@users.noreply.github.com>
2025-09-24 19:32:41 -0300

    Enhance testePilha with capacity change tests
    Added tests for inserting and removing elements from both stacks with and without capacity changes.

--- PilhaRubroNegra/testePilha.java
+++ PilhaRubroNegra/testePilha.java
@@ -1,7 +1,7 @@
 public class testePilha {
 
-	public static void main(String[] args) {		
-		Integer[] b = new Integer[1];		
+	public static void main(String[] args) {	
+		// Teste inserindo e retirando elementos das duas pilhas sem precisar alterar a capacidade
 		PilhaRubroNegra pp=new PilhaRubroNegra(10);
 		System.out.println("Inserindo Pilha Vermelha");
 		for(int f=0;f<5;f++){
@@ -23,5 +23,19 @@ public class testePilha {
 			  System.out.print(f);
 			  System.out.println(" - "+pp.popP());
 		}
+		// Teste inserindo e retirando elementos das duas pilhas alterando a capacidade
+		System.out.println("Inserindo Pilha Vermelha");
+		for(int f=0;f<5;f++){
+		  System.out.println(f);		  
+		  pp.pushV(f);
+		}
+		System.out.println("Inserindo Pilha Preta");
+		for(int f=0;f<10;f++){
+		  System.out.println(f);		  
+		  pp.pushP(f);
+		}
+		System.out.println("Tamanho Atual da Pilha Vermelha: "+pp.sizeV());
+		System.out.println("Tamanho Atual da Pilha Preta: "+pp.sizeP());
+
 	}
 }
