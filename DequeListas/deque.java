655e2751fa68710c56d7b2393d80d75a679f8408
deborasamara <deborasamara.ro@gmail.com>
2025-10-29 18:29:30 -0300

    docs

--- DequeListas/deque.java
+++ DequeListas/deque.java
@@ -1,1 +1,2 @@
+// Implemente um Deque através de uma lista duplamente encadeada e também através de uma lista simplesmente encadeada, Após fazer as implementações do Deque, faça uma comparação das duas implementações utilizando a notação do Big O para os métodos das duas implementações..
 
