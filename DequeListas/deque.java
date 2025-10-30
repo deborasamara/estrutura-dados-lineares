eb043fb919f248734afea3552bd0799147dc5747
deborasamara <deborasamara.ro@gmail.com>
2025-10-30 18:29:54 -0300

    feat: implement No class for Deque with element and next node attributes

--- DequeListas/deque.java
+++ DequeListas/deque.java
@@ -1,2 +1,22 @@
 // Implemente um Deque através de uma lista duplamente encadeada e também através de uma lista simplesmente encadeada, Após fazer as implementações do Deque, faça uma comparação das duas implementações utilizando a notação do Big O para os métodos das duas implementações..
 
+public class No{
+    private Object elemento;
+
+    private No proximo;
+
+    public Object getElemento() {
+        return elemento;
+    }
+
+    public No getElemento() {
+        return elemento;
+    }
+
+    public void setElemento(Object o) {
+        elemento = o;
+    }
+
+    
+
+}
