4e431d2e33066e028d1ad5c6df1dedf514048265
Débora Rodrigues <50723433+deborasamara@users.noreply.github.com>
2025-11-17 17:30:50 -0300

    Update deque.java

--- DequeListas/deque.java
+++ DequeListas/deque.java
@@ -1,21 +1,53 @@
 // Implemente um Deque através de uma lista duplamente encadeada e também através de uma lista simplesmente encadeada, Após fazer as implementações do Deque, faça uma comparação das duas implementações utilizando a notação do Big O para os métodos das duas implementações..
+public class DequeDL {
+    private static class No{
+    
+    private Object element;
 
+    private No proximo, anterior;
 
-// Deque através de uma lista duplamente encadeada
-public class No{
-    private Object elemento;
-
-    private No proximo;
-
-    public Object getElemento() {
-        return elemento;
+    // Construtor
+    public No(Object o, No prox, No ant){
+        element = o;
+        proximo = prox; 
+        anterior = ant;
     }
 
-    public No getElemento() {
-        return elemento;
+    // Getters e Setters 
+    public No getProximo() {
+        return proximo;
+    }
+    public void setProximo(No n) {
+        proximo = n;
+    }
+    public No getAnterior() {
+        return anterior;
+    } 
+
+    public void setAnterior(No n) {
+        anterior = n;
+    }
+    // Hearder e Trailer
+    private No header, trailer;
+
+    private int size = 0;
+    
     }
 
-    public void setElemento(Object o) {
-        elemento = o;
-    }
+    inserirInicio(object):
+
+    object removerInicio():
+
+    inserirFim(object):
+
+    object removerFim():
+
+    object primeiro():
+
+    object ultimo():
+    
+    int tamanho():
+    
+    boolean estaVazia():
+
 }
