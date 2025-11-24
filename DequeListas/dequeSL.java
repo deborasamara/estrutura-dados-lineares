b7913c177a1195a6a9415441fd96d20f69099d76
deborasamara <deborasamara.ro@gmail.com>
2025-11-23 23:21:01 -0300

    feature: implementar métodos do deque simplesmente ligado

--- DequeListas/dequeSL.java
+++ DequeListas/dequeSL.java
@@ -1,5 +1,57 @@
 // Implemente um Deque através de uma lista duplamente encadeada e também através de uma lista simplesmente encadeada, Após fazer as implementações do Deque, faça uma comparação das duas implementações utilizando a notação do Big O para os métodos das duas implementações..
 public class DequeSL {
-    private static class No{
+    private Integer size; 
+    private SLNo head;
+
+     public DequeSL(){
+        size = 0;
+        head = null;
     }
+
+    // insere elemento o no começo do Deque
+    public void inserirInicio(Object o){
+         // o head sempre vai ser o primeiro nó
+        SLNo novo_no = new SLNo(o, head);
+        SLNo aux = head;
+        head = novo_no;
+        if(estaVazia()){
+            novo_no.setNext(null);
+        }
+        novo_no.setNext(aux);
+        size++;
+    }
+
+    // remove e retorna o elemento do começo do deque. Ocorre um erro se o deque estiver vazio. 
+    public Object removerInicio(){
+        if(estaVazia()){
+            throw new DequeEmptyException("Deque vazio");
+        }
+        SLNo primeiro = head;
+
+        if(size == 1){
+            head = null;
+            head.setNext(null);
+        }else{
+            head = head.getNext();
+        }
+        size--;
+        return primeiro.getElement();
+    }
+
+    public void inserirFim(Object o){}
+
+    public Object removerFim(){}
+
+    public Object primeiro(){}
+
+    public Object ultimo (){}
+
+    public int tamanho(){
+        return size;
+    }
+
+    public boolean estaVazia(){
+        return (tamanho()==0);
+    }
+    
 }
