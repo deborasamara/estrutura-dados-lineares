b0556b15e832e565e0a5051fb9c5026978919703
Débora Rodrigues <50723433+deborasamara@users.noreply.github.com>
2025-11-24 17:33:31 -0300

    Implement main method for DequeDL testing
    Added a main method to test DequeDL functionality.

--- DequeListas/testeDequeDL.java
+++ DequeListas/testeDequeDL.java
@@ -1,1 +1,9 @@
 
+public class testeDequeDL{
+    public static void main(String[]args){
+        DequeDL deque = new DequeDL();
+        System.out.println("Criando Deque");
+        deque.inserirInicio(10);
+        System.out.println(deque.ultimo());
+    }
+}
