ff788942ccaecda34b8983f7ac0f7564795332ad
deborasamara <deborasamara.ro@gmail.com>
2026-03-30 18:16:05 -0300

    refactor: streamline methods and improve exception handling in PilhaRubroNegra class

--- PilhaRubroNegra/PilhaRubroNegra.java
+++ PilhaRubroNegra/PilhaRubroNegra.java
@@ -1,136 +1,107 @@
-public class PilhaRubroNegra{
-    // Variáveis
-    public int capacidade;
-    public Object[] a;
-    public int tVermelho, tPreto;
+public class PilhaRubroNegra {
+// Variáveis
+public int capacidade;
+public Object[] a;
+public int tVermelho, tPreto;
+// Construtor
+public PilhaRubroNegra(int capacidade) {
+this.capacidade = capacidade;
+tVermelho = -1;
+tPreto = capacidade;
+a = new Object[capacidade];
+}
+public int capacidade() {
+return capacidade;
+}
+public void aumentarPilha() {
+Object[] array_aux = new Object[capacidade * 2];
+// Copia pilha vermelha
+for (int i = 0; i < sizeV(); i++) {
+array_aux[i] = a[i];
+}
+// Novo topo da pilha preta
+int novoTPreto = capacidade * 2 - sizeP();
+// Copia pilha preta corretamente
+for (int j = 0; j < sizeP(); j++) {
+array_aux[novoTPreto + j] = a[tPreto + j];
+}
+a = array_aux;
+capacidade = capacidade * 2;
+tPreto = novoTPreto;
+}
 
-    // Construtor
-    public PilhaRubroNegra(int capacidade){
-        this.capacidade=capacidade;
-        tVermelho = -1;
-        tPreto = capacidade;
-        a = new Object[capacidade];
-    }
+public void diminuirPilha() {
+Object[] array_aux = new Object[capacidade / 2];
+// Copia pilha vermelha
+for (int i = 0; i < sizeV(); i++) {
+array_aux[i] = a[i];
+}
+int novoTPreto = (capacidade / 2) - sizeP();
+// Copia pilha preta corretamente
+for (int j = 0; j < sizeP(); j++) {
+array_aux[novoTPreto + j] = a[tPreto + j];
+}
+a = array_aux;
+capacidade = capacidade / 2;
+tPreto = novoTPreto;
+}
+public void verificaCheio() {
+if ((sizeV() + sizeP()) == capacidade) {
+aumentarPilha();
+}
+}
+public void verificaUmTerco() {
+if ((sizeV() + sizeP()) <= capacidade / 3) {
+diminuirPilha();
+}
+}
+public int sizeV() {
+return tVermelho + 1;
+}
+public int sizeP() {
+return capacidade - tPreto;
+}
+public boolean isEmptyV() {
+return tVermelho == -1;
+}
+public boolean isEmptyP() {
+return tPreto == capacidade;
+}
 
-    // Métodos
-    
-    //Verifica capacidade
-    public int capacidade(){
-        return capacidade;
-    }
-
-    public void aumentarPilha(){
-        // Declara array auxiliar 
-        Object[] array_aux = new Object[capacidade*2];
-
-        // preenche pilha vermelha
-        for(int i = 0; i<(sizeV());i++){
-            array_aux[i]= a[i];
-        }
-        // preenche pilha preta
-
-        // novo Tpreto
-        int novotPreto = capacidade*2 - sizeP();
-
-        for(int i = novotPreto; i<(capacidade*2); i++){
-            array_aux[i]=a[i-capacidade];
-        }
-        a = array_aux;
-        this.capacidade = capacidade*2;
-        tPreto = novotPreto;
-    }
-
-    public void diminuirPilha(){
-        // Declara array auxiliar 
-        Object[] array_auxd = new Object[capacidade/2];
-
-        // copia pilha vermelha
-         for(int i = 0; i<(sizeV());i++){
-            array_auxd[i]= a[i];
-        }
-
-        // Novo Tp = capacidade/2 - TamanhoPilhaPreta
-         int novotPreto = capacidade/2 - sizeP();
-        // copia pilha preta
-        for(int i = novotPreto; i<(capacidade/2); i++){
-            array_auxd[i] = a[i+(capacidade/2)];
-        }
-         a = array_auxd;
-         this.capacidade = capacidade/2;
-         tPreto = novotPreto;
-    }
-
-    public void verificaCheio(){
-        if((sizeV() + sizeP()) == capacidade){
-            aumentarPilha();
-        }
-    }
-
-    public void verificaUmTerco(){
-        // System.out.println("\n");
-        // System.out.println("Verificando um terço. Capacidade:" + capacidade +"Tamanho Vermelha:" + sizeV() + " Tamanho Preta:" + sizeP() + " Resultado:" + ((sizeV() + sizeP()) <= capacidade/3));
-        if((sizeV() + sizeP()) <= capacidade/3){
-            diminuirPilha();
-        }
-    }
-
-    public int sizeV(){
-        return tVermelho+1;
-    }
-
-    public int sizeP(){
-        return capacidade-tPreto;
-    }
-
-    public boolean isEmptyV(){
-        return tVermelho == -1;
-    }
-
-    public boolean isEmptyP(){
-        return tPreto == capacidade;
-    }
-
-    public Object topV() throws PilhaVaziaExcecao{
-        if(isEmptyV()){
-            throw new PilhaVaziaExcecao("A pilha vermelha tá vazia!");
-        }
-        return a[tVermelho];
-    }
-
-    public Object topP(){
-        if(isEmptyP()){
-            throw new PilhaVaziaExcecao("A pilha preta tá vazia!");
-        }
-        return a[tPreto];
-
-    }
-
-    public void pushV(Object o){
-        a[++tVermelho]=o;
-        verificaCheio();
-    }
-
-    public void pushP(Object o){
-        a[--tPreto]=o;
-        verificaCheio();
-    }
-
-    public Object popV() throws PilhaVaziaExcecao{
-        if(isEmptyV()){
-            throw new PilhaVaziaExcecao("A pilha vermelha tá vazia!!");
-        }
-        Object r=a[tVermelho--];
-        verificaUmTerco();
-        return r;
-    }
-
-    public Object popP() throws PilhaVaziaExcecao{
-        if(isEmptyP()){
-            throw new PilhaVaziaExcecao("A pilha preta tá vazia!!");
-        }
-        Object l=a[tPreto++];
-        verificaUmTerco();
-        return l;
-    }
-
+public Object topV() {
+if (isEmptyV()) {
+throw new RuntimeException("A pilha vermelha tá vazia!");
+}
+return a[tVermelho];
+}
+public Object topP() {
+if (isEmptyP()) {
+throw new RuntimeException("A pilha preta tá vazia!");
+}
+return a[tPreto];
+}
+public void pushV(Object o) {
+a[++tVermelho] = o;
+verificaCheio();
+}
+public void pushP(Object o) {
+a[--tPreto] = o;
+verificaCheio();
+}
+public Object popV() {
+if (isEmptyV()) {
+throw new RuntimeException("A pilha vermelha tá vazia!!");
+}
+Object r = a[tVermelho--];
+verificaUmTerco();
+return r;
+}
+public Object popP() {
+if (isEmptyP()) {
+throw new RuntimeException("A pilha preta tá vazia!!");
+}
+Object l = a[tPreto++];
+verificaUmTerco();
+return l;
+}
 }
