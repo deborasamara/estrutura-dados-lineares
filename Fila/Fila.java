5cbbfeb9f9fad0b33a2616bf8e8a8083f008d86d
Débora Rodrigues <50723433+deborasamara@users.noreply.github.com>
2026-04-09 14:34:11 +0000

    feat: implementar a classe Fila utilizando lista simplesmente encadeada

---
+++ Fila/Fila.java
@@ -0,0 +1,15 @@
+//Implemente os TADs Fila utilizando a estrutura de dados concreta lista simplesmente encadeada.
+public class Fila{
+    // variáveis
+    private int i = 0, f = 0, N, incremento;
+    private object O[];
+
+    // construtores
+    public Fila(int N, int incremento){
+        O = new Object[N];
+        this.N = N;
+        this.incremento = incremento;
+    }
+    // métodos 
+
+}
