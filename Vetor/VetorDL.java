5a80368e086382b691992624a5c3442b4aef130c
deborasamara <deborasamara.ro@gmail.com>
2026-05-06 18:19:52 -0300

    feat: expand VetorDL class with access and modification methods

--- Vetor/VetorDL.java
+++ Vetor/VetorDL.java
@@ -1,8 +1,63 @@
 public class VetorDL{
-    DLNo head, tail;
-    
-    public VetorDL(){
+    DLNo header, tail;
+    int size = 0;
 
+    // construtor 
+    public VetorDL(){
+        header = new DLNo();
+        header.setProximo(null);
+        header.setAnterior(null);
+        tail = header;
     }
+    // Métodos de acesso  
+     public int size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    public Object atRank(int r){
+        DLNo atual = header;
+        for(int i = 0; i<=r; i++){
+            atual = atual.getProximo();
+        }
+        return atual.getElement();
+    }
+    // Métodos de modificação 
+    public Object replaceAtRank(int r, Object o){
+        DLNo atual = header;
+        for(int i = 0; i<=r; i++){
+            atual = atual.getProximo();
+        }
+        Object temp = atual.getElement();
+        atual.setElement(o);
+        return temp;
+    } 
+    public Object removeAtRank(int r){
+        DLNo atual = header;
+         for(int i = 0; i<=r; i++){
+            atual = atual.getProximo();
+        }
+        Object temp = atual.getElement();
+        atual.setElement(null);
+        atual.getAnterior().setProximo(atual.getProximo());
+        atual.getProximo().setAnterior(atual.getAnterior());
+        size--;
+        return temp;
+    }
+    public void insertAtRank(int r, Object o){
+        DLNo atual = header; 
+        for(int i = 0; i<=r; i++){
+            atual = atual.getProximo();
+        }
+        DLNo novo = new DLNo();
+        novo.setElement(o);
+        atual.getAnterior().setProximo(novo);
+        novo.setAnterior(atual.getAnterior());
+        novo.setProximo(atual);
+        atual.setAnterior(novo);
+        size++;
+    }
+    
 
 }
