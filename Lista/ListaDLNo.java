411a7a8a963a580c643f2ca777dfc601909bf06b
deborasamara <deborasamara.ro@gmail.com>
2026-05-09 21:47:08 -0300

    feat: implement constructor for ListaDLNo class to initialize header and tail

--- Lista/ListaDLNo.java
+++ Lista/ListaDLNo.java
@@ -1,4 +1,18 @@
 public class ListaDLNo {
     private DLNo header, tail;
     private int size;
+    // Construtor
+    public ListaDLNo(){
+        size = 0;
+        header = new DLNo(null, null, null);
+        tail = new DLNo(null, null, null);
+        header.setProximo(tail);
+        tail.setAnterior(header);
+    }
+
+    // Métodos de acesso  
+
+    // Métodos de modificação
+
+    // 
 }
