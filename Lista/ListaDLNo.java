ce59cc9d516385dd2787a1ec0c43e624c6b6e80d
deborasamara <deborasamara.ro@gmail.com>
2026-05-09 22:04:18 -0300

    refactor: improve formatting and organization of ListaDLNo class methods

--- Lista/ListaDLNo.java
+++ Lista/ListaDLNo.java
@@ -1,8 +1,9 @@
 public class ListaDLNo {
     private DLNo header, tail;
     private int size;
+
     // Construtor
-    public ListaDLNo(){
+    public ListaDLNo() {
         size = 0;
         header = new DLNo(null, null, null);
         tail = new DLNo(null, null, null);
@@ -10,9 +11,38 @@ public class ListaDLNo {
         tail.setAnterior(header);
     }
 
-    // Métodos de acesso  
+    // Métodos genéricos
+    public int size() {
+        return size;
+    }
 
-    // Métodos de modificação
+    public boolean isEmpty(){
+        return size == 0;
+    }
 
-    // 
+    // Métodos de acesso
+    first()
+    last()
+    before(p)
+    after(p)
+
+    // Métodos de atualização
+    replaceElement(n, o)
+    swapElements(n, q)
+
+    insertBefore(n, o),
+    insertAfter(n, o),
+
+    insertFirst(o),
+    insertLast(o)
+    remove(n)
+
+    // Métodos de Fila
+    public boolean isFirst( n){
+        return n 
+
+    }
+    public boolean isLast(n){
+
+    }
 }
